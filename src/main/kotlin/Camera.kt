e462bf55bffdd32f1dcf05f755fad12599fbd5ee
Alexandr Isaev <isaevalex199@gmail.com>
2021-01-16 10:05:19 +0300

    feat: camera

---
+++ src/main/kotlin/Camera.kt
@@ -0,0 +1,133 @@
+import com.hackoeur.jglm.Mat4
+import com.hackoeur.jglm.Matrices.lookAt
+import com.hackoeur.jglm.Vec3
+import com.hackoeur.jglm.support.FastMath.*
+
+// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+enum class CameraMovement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+}
+
+// Default camera values
+const val YAW = -90.0f
+const val PITCH = 0.0f
+const val SPEED = 3.0f
+const val SENSITIVTY = 0.25f
+const val ZOOM = 45.0f
+
+
+// An abstract camera class that processes input and calculates the corresponding Eular Angles, Vectors and Matrices for use in OpenGL
+class Camera {
+    // Camera Attributes
+    var position: Vec3
+    var front: Vec3
+    lateinit var up: Vec3
+    lateinit var right: Vec3
+    var worldUp: Vec3
+
+    // Eular Angles
+    var yaw: Float
+    var pitch: Float
+
+    // Camera options
+    var movementSpeed: Float
+    var mouseSensitivity: Float
+    var zoom: Float
+
+
+    // Constructor with vectors
+    constructor(
+        position: Vec3 = Vec3(0.0f, 0.0f, 0.0f),
+        up: Vec3 = Vec3(0.0f, 1.0f, 0.0f),
+        yaw: Float = YAW,
+        pitch: Float = PITCH
+    ) {
+        front = Vec3(0.0f, 0.0f, -1.0f)
+        movementSpeed = SPEED
+        mouseSensitivity = SENSITIVTY
+        zoom = ZOOM
+        this.position = position
+        worldUp = up
+        this.yaw = yaw
+        this.pitch = pitch
+        updateCameraVectors()
+    }
+
+    // Constructor with scalar values
+    constructor(posX: Float, posY: Float, posZ: Float, upX: Float, upY: Float, upZ: Float, yaw: Float, pitch: Float) {
+        front = Vec3(0.0f, 0.0f, -1.0f)
+        movementSpeed = SPEED
+        mouseSensitivity = SENSITIVTY
+        zoom = ZOOM
+        position = Vec3(posX, posY, posZ)
+        worldUp = Vec3(upX, upY, upZ)
+        this.yaw = yaw
+        this.pitch = pitch
+        updateCameraVectors()
+    }
+
+    // Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+    fun getViewMatrix(): Mat4 {
+        return lookAt(position, position.add(front), up)
+    }
+
+    // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    fun processKeyboard(direction: CameraMovement, deltaTime: Float) {
+        val velocity = movementSpeed * deltaTime
+        if (direction == CameraMovement.FORWARD)
+            position = position.add(front.multiply(velocity))
+        if (direction == CameraMovement.BACKWARD)
+            position = position.subtract(front.multiply(velocity))
+        if (direction == CameraMovement.LEFT)
+            position = position.subtract(right.multiply(velocity))
+        if (direction == CameraMovement.RIGHT)
+            position = position.add(right.multiply(velocity))
+    }
+
+    // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    fun processMouseMovement(xoffset: Float, yoffset: Float, constrainPitch: Boolean = true) {
+        val xoffset1 = xoffset * mouseSensitivity
+        val yoffset1 = yoffset * mouseSensitivity
+
+        yaw += xoffset1
+        pitch += yoffset1
+
+        // Make sure that when pitch is out of bounds, screen doesn't get flipped
+        if (constrainPitch) {
+            if (pitch > 89.0f)
+                pitch = 89.0f
+            if (pitch < -89.0f)
+                pitch = -89.0f
+        }
+
+        // Update Front, Right and Up Vectors using the updated Eular angles
+        updateCameraVectors()
+    }
+
+    // Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+    fun processMouseScroll(yoffset: Float) {
+        if (zoom >= 1.0f && zoom <= 45.0f)
+            zoom -= yoffset
+        if (zoom <= 1.0f)
+            zoom = 1.0f
+        if (zoom >= 45.0f)
+            zoom = 45.0f
+    }
+
+    private fun updateCameraVectors() {
+        // Calculate the new Front vector
+        val front = Vec3(
+            (cos(toRadians(yaw.toDouble())) * cos(toRadians(pitch.toDouble()))).toFloat(),
+            sin(toRadians(pitch.toDouble())).toFloat(),
+            (sin(toRadians(yaw.toDouble())) * cos(toRadians(pitch.toDouble()))).toFloat()
+        )
+        this.front = front.unitVector
+        // Also re-calculate the Right and Up vector
+        right =
+            this.front.cross(worldUp).unitVector  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        up = right.cross(this.front).unitVector
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import com.hackoeur.jglm.Mat4
 import com.hackoeur.jglm.Matrices.perspective
 import com.hackoeur.jglm.Vec3
+import com.hackoeur.jglm.Vec4
+import com.hackoeur.jglm.support.FastMath.*
 import org.lwjgl.BufferUtils
 import org.lwjgl.Version
 import org.lwjgl.glfw.Callbacks.glfwFreeCallbacks
@@ -8,13 +10,16 @@ import org.lwjgl.glfw.GLFW.*
 import org.lwjgl.glfw.GLFWErrorCallback
 import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL30.*
-import org.lwjgl.stb.STBImage.*
+import org.lwjgl.stb.STBImage.stbi_failure_reason
+import org.lwjgl.stb.STBImage.stbi_load
 import org.lwjgl.system.MemoryStack.stackPush
 import org.lwjgl.system.MemoryUtil.NULL
 import java.io.File
+import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.FloatBuffer
 import java.nio.IntBuffer
+import kotlin.math.pow
 
 
 private var window: Long = 0
@@ -64,6 +69,14 @@ var vertices: FloatBuffer = createFloatBuffer(
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
     )
 )
+var camera = Camera(Vec3(0.0f, 0.0f, 3.0f))
+var keys = BooleanArray(1024) { false }
+var firstMouse = true
+
+var deltaTime = 0.0f
+var lastFrame = 0.0f
+var lastX = 400.0
+var lastY = 300.0
 
 //var indices = createIntBuffer(
 //    intArrayOf(
@@ -87,8 +100,8 @@ var cubePositions: Array<Vec3> = arrayOf(
 
 lateinit var shader: Shader
 
-const val WIDTH = 640
-const val HEIGHT = 480
+const val WIDTH = 800
+const val HEIGHT = 600
 
 fun main() {
     println("Hello LWJGL " + Version.getVersion() + "!")
@@ -115,9 +128,46 @@ fun init() {
     window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL)
     if (window == NULL) throw RuntimeException("Failed to create the GLFW window")
 
+    println("W $GLFW_KEY_W")
+    println("A $GLFW_KEY_A")
+    println("S $GLFW_KEY_S")
+    println("D $GLFW_KEY_D")
     glfwSetKeyCallback(window) { window: Long, key: Int, scancode: Int, action: Int, mods: Int ->
-        if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) glfwSetWindowShouldClose(window, true)
+        println(key)
+        //cout << key << endl;
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true)
+        if (key in 0..1023) {
+            if (action == GLFW_PRESS)
+                keys[key] = true
+            else if (action == GLFW_RELEASE)
+                keys[key] = false
+        }
     }
+
+    glfwSetCursorPosCallback(window) { l: Long, xPos: Double, yPos: Double ->
+        if (firstMouse) {
+            lastX = xPos
+            lastY = yPos
+            firstMouse = false
+        }
+
+        val xoffset = xPos - lastX
+        val yoffset = lastY - yPos // Reversed since y-coordinates go from bottom to left
+
+
+        lastX = xPos
+        lastY = yPos
+
+        camera.processMouseMovement(xoffset.toFloat(), yoffset.toFloat())
+    }
+
+    glfwSetScrollCallback(window) { _: Long, _: Double, yOffset: Double ->
+        camera.processMouseScroll(yOffset.toFloat())
+    }
+
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED)
 
     // Get the thread stack and push a new frame
     stackPush().use { stack ->
@@ -154,7 +204,7 @@ private fun loop() {
     glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
     glVertexAttribPointer(0, 3, GL_FLOAT, false, 5 * 4, 0)
     glEnableVertexAttribArray(0)
-    glVertexAttribPointer(1, 2, GL_FLOAT, false, 5 * 4, 3*4)
+    glVertexAttribPointer(1, 2, GL_FLOAT, false, 5 * 4, 3 * 4)
     glEnableVertexAttribArray(1)
     glBindBuffer(GL_ARRAY_BUFFER, 0)
     glBindVertexArray(0)
@@ -167,29 +217,38 @@ private fun loop() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
 
-    val imagePath = object {}.javaClass.getResource("texture/container.png")
-    val imageBuffer = createByteBuffer(File(imagePath.toURI()).readBytes())
-    var image: ByteBuffer
+    val imagePath = object {}.javaClass.getResource("texture/smile.png")
+    val imageFile = File(imagePath.toURI())
+//    val imageBuffer = createByteBuffer(imageFile.readBytes())
+//    var image: ByteBuffer
     var imageWidth: Int
     var imageHeight: Int
-    var imageComponents: Int
+//    var imageComponents: Int
 
-    stackPush().use { stack ->
-        val width = stack.mallocInt(1)
-        val height = stack.mallocInt(1)
-        val components = stack.mallocInt(1)
+//    stackPush().use { stack ->
+//        val width = stack.mallocInt(1)
+//        val height = stack.mallocInt(1)
+//        val components = stack.mallocInt(1)
+//
+//        if (!stbi_info_from_memory(imageBuffer, width, height, components)) {
+//            throw RuntimeException("Failed to read image information: " + stbi_failure_reason())
+//        } else {
+//            println("OK with reason: " + stbi_failure_reason())
+//        }
+//        // Decode the image
+//        image = stbi_load_from_memory(imageBuffer, width, height, components, 0)!!
+//        imageWidth = width[0]
+//        imageHeight = height[0]
+//        imageComponents = components[0]
+//    }
 
-        if (!stbi_info_from_memory(imageBuffer, width, height, components)) {
-            throw RuntimeException("Failed to read image information: " + stbi_failure_reason())
-        } else {
-            println("OK with reason: " + stbi_failure_reason())
-        }
-        // Decode the image
-        image = stbi_load_from_memory(imageBuffer, width, height, components, 0)!!
-        imageWidth = width[0]
-        imageHeight = height[0]
-        imageComponents = components[0]
-    }
+    val w = BufferUtils.createIntBuffer(1)
+    val h = BufferUtils.createIntBuffer(1)
+    val comp = BufferUtils.createIntBuffer(1)
+    val buffer: ByteBuffer = stbi_load(imageFile.toString(), w, h, comp, 3) ?: throw IOException(stbi_failure_reason())
+
+    imageWidth = w[0]
+    imageHeight = h[0]
 
     println("Width ${imageWidth}, Height ${imageHeight}, Data ${0}")
 
@@ -203,13 +262,20 @@ private fun loop() {
         0,
         GL_RGB,
         GL_UNSIGNED_BYTE,
-        image
+        createByteBuffer(buffer)
     )
     glGenerateMipmap(GL_TEXTURE_2D)
     glBindTexture(GL_TEXTURE_2D, 0)
 
+    glEnable(GL_DEPTH_TEST)
+
     while (!glfwWindowShouldClose(window)) {
+        val currentFrame = glfwGetTime().toFloat()
+        deltaTime = currentFrame - lastFrame
+        lastFrame = currentFrame
+
         glfwPollEvents()
+        doMovement()
         glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
 
         glActiveTexture(GL_TEXTURE0)
@@ -217,8 +283,8 @@ private fun loop() {
         glUniform1i(glGetUniformLocation(shader.program, "ourTexture"), 0)
 
         // Create transformations
-        val view = Mat4.MAT4_IDENTITY.translate(Vec3(0.0f, 0.0f, -3.0f))
-        val projection: Mat4 = perspective(45.0f, WIDTH / HEIGHT.toFloat(), 0.1f, 100.0f)
+        val view = camera.getViewMatrix()
+        val projection: Mat4 = perspective(camera.zoom, WIDTH / HEIGHT.toFloat(), 0.1f, 100.0f)
         // Get their uniform location
         val modelLoc = glGetUniformLocation(shader.program, "model")
         val viewLoc = glGetUniformLocation(shader.program, "view")
@@ -230,10 +296,11 @@ private fun loop() {
 
         glBindVertexArray(vao)
         for (i in 0..9) {
-            // Calculate the model matrix for each object and pass it to shader before drawing
-            val model = Mat4.MAT4_IDENTITY.translate(cubePositions[i].unitVector)
-//            val angle = 20.0f * i
-//            model = model.multiply(rotate(angle, Vec3(1.0f, 0.3f, 0.5f)))
+            val angle = 20.0f * i
+            val model = Mat4.MAT4_IDENTITY
+                .translate(cubePositions[i])
+                .rotate(toRadians(angle.toDouble()).toFloat(), Vec3(1.0f, 0.3f, 0.5f))
+//                .scale(Vec3(0.1f * i, 0.3f * i, 0.2f * i))
             glUniformMatrix4fv(modelLoc, false, createFloatMat4(model.buffer))
 
             glDrawArrays(GL_TRIANGLES, 0, 36)
@@ -242,6 +309,14 @@ private fun loop() {
 
         glfwSwapBuffers(window)
     }
+}
+
+fun doMovement() {
+    // Camera controls
+    if (keys[GLFW_KEY_W]) camera.processKeyboard(CameraMovement.FORWARD, deltaTime)
+    if (keys[GLFW_KEY_S]) camera.processKeyboard(CameraMovement.BACKWARD, deltaTime)
+    if (keys[GLFW_KEY_A]) camera.processKeyboard(CameraMovement.LEFT, deltaTime)
+    if (keys[GLFW_KEY_D]) camera.processKeyboard(CameraMovement.RIGHT, deltaTime)
 }
 
 fun createFloatBuffer(data: FloatArray): FloatBuffer {
@@ -253,6 +328,13 @@ fun createFloatBuffer(data: FloatArray): FloatBuffer {
 
 fun createFloatMat4(data: FloatBuffer): FloatBuffer {
     val buffer = BufferUtils.createFloatBuffer(16)
+    buffer.put(data)
+    buffer.flip()
+    return buffer
+}
+
+fun createByteBuffer(data: ByteBuffer): ByteBuffer {
+    val buffer = BufferUtils.createByteBuffer(data.capacity())
     buffer.put(data)
     buffer.flip()
     return buffer
@@ -270,4 +352,44 @@ fun createByteBuffer(data: ByteArray): ByteBuffer {
     buffer.put(data)
     buffer.flip()
     return buffer
+}
+
+fun Mat4.rotate(phi: Float, axis: Vec3): Mat4 {
+    val cos = cos(phi.toDouble()).toFloat()
+    val oneMinusCos = 1.0f - cos
+    val sin = sin(phi.toDouble()).toFloat()
+    val x = axis.x
+    val y = axis.y
+    val z = axis.z
+    val v1 = Vec4(
+        cos + x.pow(2) * oneMinusCos,
+        (x * y) * oneMinusCos - z * sin,
+        (x * z) * oneMinusCos + y * sin,
+        0f
+    )
+    val v2 = Vec4(
+        (y * x) * oneMinusCos + z * sin,
+        cos + y.pow(2) * oneMinusCos,
+        (y * z) * oneMinusCos - x * sin,
+        0.0F
+    )
+    val v3 = Vec4(
+        (z * x) * oneMinusCos - y * sin,
+        (z * y) * oneMinusCos + x * sin,
+        cos + z.pow(2) * oneMinusCos,
+        0.0F
+    )
+    val v4 = Vec4(0.0F, 0.0F, 0.0F, 1.0F)
+    return this.multiply(Mat4(v1, v2, v3, v4))
+}
+
+fun Mat4.scale(axis: Vec3): Mat4 {
+    return this.multiply(
+        Mat4(
+            Vec4(axis.x, 0f, 0f, 0f),
+            Vec4(0f, axis.y, 0f, 0f),
+            Vec4(0f, 0f, axis.z, 0f),
+            Vec4(0f, 0f, 0f, 1f)
+        )
+    )
 }
